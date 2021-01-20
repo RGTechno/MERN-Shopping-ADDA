2a43de57fa06ad9becb18a28b7b467aea16beaf6
Rahul Gandhi <65856344+RGTechno@users.noreply.github.com>
2021-01-20 13:09:32 +0530

    ratings

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -9,8 +9,8 @@ const HomeScreen = () => {
       <h3>Latest Products</h3>
       <Row>
         {products.map((product) => (
-          <Col sm={12} md={6} lg={4} xl={3}>
-            <Product product={product} key={product._id} />
+          <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
+            <Product product={product} />
           </Col>
         ))}
       </Row>
