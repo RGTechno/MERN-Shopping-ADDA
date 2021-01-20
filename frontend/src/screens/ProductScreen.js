a03b63f6549b47f44ddb8433f547251bd94ab3e6
Rahul Gandhi <65856344+RGTechno@users.noreply.github.com>
2021-01-20 14:03:18 +0530

    product screen

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,7 +1,59 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
+import products from '../products'
+import Rating from '../components/Rating'
 
-const ProductScreen = () => {
-  return <div>Product</div>
+const ProductScreen = ({ match }) => {
+  const product = products.find((p) => p._id === match.params.id)
+  return (
+    <>
+      <Link to='/' className='my-3 btn btn-outline-dark rounded'>
+        Go Back
+      </Link>
+      <Row>
+        <Col md={6}>
+          <Image src={product.image} alt={product.name} fluid />
+        </Col>
+        <Col md={3}>
+          <ListGroup variant='flush'>
+            <ListGroup.Item>
+              <h4>{product.name}</h4>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Rating
+                value={product.rating}
+                text={`${product.numReviews} reviews`}
+              />
+            </ListGroup.Item>
+            <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
+            <ListGroup.Item>Desription: {product.description}</ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col md={3}>
+          <Card>
+            <ListGroup variant='flush'>
+              <ListGroup.Item>
+                <h5>Price: ${product.price}</h5>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                Status: {product.countInStock > 0 ? 'In Stock' : 'Out Of Stock'}
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Button
+                  className='btn-block'
+                  type='button'
+                  disabled={product.countInStock === 0}
+                >
+                  Add To Cart
+                </Button>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
+    </>
+  )
 }
 
 export default ProductScreen
