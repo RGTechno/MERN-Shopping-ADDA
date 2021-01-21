407212ce049a796b5027ca908b3b92cb9c9258c0
Rahul Gandhi <65856344+RGTechno@users.noreply.github.com>
2021-01-21 13:00:25 +0530

    explanatory

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,8 @@
 const express = require('express')
+const dotenv = require('dotenv')
 const products = require('./data/products')
+
+dotenv.config()
 
 const app = express()
 
@@ -11,6 +14,8 @@ app.get('/api/products/:id', (req, res) => {
   res.json(product)
 })
 
-app.listen(5000, () => {
-  console.log('server started on http://localhost:5000')
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+  console.log(`server started in ${process.env.NODE_MODE} mode on http://localhost:${PORT}`)
 })
