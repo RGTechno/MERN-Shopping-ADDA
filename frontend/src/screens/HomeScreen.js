539b40c83dc5bee7f9baaf7235341f659fd40ea3
Rahul Gandhi <65856344+RGTechno@users.noreply.github.com>
2021-01-22 14:01:10 +0530

    loader and err msg

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -3,6 +3,8 @@ import { useDispatch, useSelector } from 'react-redux'
 import { Col, Row } from 'react-bootstrap'
 import Product from '../components/Product'
 import { listProduct } from '../actions/productAction'
+import Loader from '../components/Loader'
+import Message from '../components/Message'
 
 const HomeScreen = () => {
   const dispatch = useDispatch()
@@ -15,9 +17,9 @@ const HomeScreen = () => {
     <>
       <h3>Latest Products</h3>
       {loading ? (
-        <h2>Loading...</h2>
+        <Loader />
       ) : error ? (
-        <h3>{error}</h3>
+        <Message variant='danger'>{error}</Message>
       ) : (
         <Row>
           {products.map((product) => (
